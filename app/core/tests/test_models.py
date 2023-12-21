793c8244663b19d6175deca547fe1dc904ccdabc
adriangabriel1000 <adriangabriel1000@gmail.com>
2023-12-21 19:12:52 +0200

    Updated code 48.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,7 +8,7 @@ from django.contrib.auth import get_user_model
 class ModelTest(TestCase):
     """Test models."""
 
-    def test_create_user_with_email_successful(delf):
+    def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
         email = 'test@example.com'
         password = 'testpass123'
